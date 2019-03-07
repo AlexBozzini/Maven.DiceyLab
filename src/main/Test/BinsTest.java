e6eec84f32d5eff97b40580ef28b03da05acae1f
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-03-07 15:13:01 -0500

    Worked on bins class/tests

--- src/main/Test/BinsTest.java
+++ src/main/Test/BinsTest.java
@@ -3,14 +3,57 @@ import org.junit.Test;
 
 public class BinsTest {
     @Test
-    public void constructorTest(){
+    public void nullaryConstructorTest(){
         // given
+        Dice dice = new Dice(2);
+        Bins bins = new Bins();
         Integer expectedLowestRoll = 2;
         Integer expectedHighestRoll = 12;
         // when
-        Bins testBin = new Bins();
+        Integer actualLowestRoll = bins.getLowestRoll();
+        Integer actualHighestRoll = bins.getHighestRoll();
         // then
-        Assert.assertEquals(expectedLowestRoll, testBin.getLowestRoll());
-        Assert.assertEquals(expectedHighestRoll, testBin.getHighestRoll());
+        Assert.assertEquals(expectedLowestRoll, actualLowestRoll);
+        Assert.assertEquals(expectedHighestRoll, actualHighestRoll);
     }
+
+    @Test
+    public void constructorTest(){
+        // given
+        Dice dice = new Dice(2);
+        Bins bins = new Bins(3, 18);
+        Integer expectedLowestRoll = 3;
+        Integer expectedHighestRoll = 18;
+        // when
+        Integer actualLowestRoll = bins.getLowestRoll();
+        Integer actualHighestRoll = bins.getHighestRoll();
+        // then
+        Assert.assertEquals(expectedLowestRoll, actualLowestRoll);
+        Assert.assertEquals(expectedHighestRoll, actualHighestRoll);
+    }
+
+    @Test
+    public void getLowestRollTest(){
+        // given
+        Bins bins = new Bins(2, 12);
+        Integer expected = 2;
+        // when
+        Integer actual = bins.getLowestRoll();
+        // then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getHighestRollTest(){
+        // given
+        Bins bins = new Bins(2, 12);
+        Integer expected = 12;
+        // when
+        Integer actual = bins.getHighestRoll();
+        // then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void storeRollTest(){}
 }
--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -2,9 +2,17 @@
 public class Bins {
     private Integer lowestRoll;
     private Integer highestRoll;
+    private Dice dice;
+
+    public Bins(Integer lowestRoll, Integer highestRoll){
+        this.lowestRoll = lowestRoll;
+        this.highestRoll = highestRoll;
+    }
+
     public Bins(){
-        this.lowestRoll = 2;
-        this.highestRoll = 12;
+        Dice dice = new Dice(2);
+        this.lowestRoll = dice.getNumberOfDice();
+        this.highestRoll = dice.getNumberOfDice() * 6;
     }
     public Integer getLowestRoll(){
         return lowestRoll;
@@ -13,4 +21,6 @@ public class Bins {
     public Integer getHighestRoll() {
         return highestRoll;
     }
+
+    public void storeRoll(){}
 }
